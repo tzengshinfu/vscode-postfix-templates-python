f24362f4cc256e5735af56365082d567d992a694
tzengshinfu <tzengshinfu@gmail.com>
2025-10-14 14:42:51 +0800

    temp save

--- src/postfixCompletionProvider.ts
+++ src/postfixCompletionProvider.ts
@@ -39,7 +39,7 @@ export class PostfixCompletionProvider implements vsc.CompletionItemProvider {
       }
       const wordRange = document.getWordRangeAtPosition(position)
       const afterDot = line.text.slice(dotIndex + 1, position.character)
-      const isHtmlLike = document.languageId === 'html'
+      const isHtmlLike = document.languageId === 'html' || document.languageId === 'postfix'
       const isCursorOnWordAfterDot = isHtmlLike
         ? /^[A-Za-z_]+$/.test(afterDot)
         : (wordRange?.start ?? position).character === dotIndex + 1
@@ -97,7 +97,8 @@ export class PostfixCompletionProvider implements vsc.CompletionItemProvider {
 
   private getHtmlLikeEmbeddedText(document: vsc.TextDocument, position: vsc.Position) {
     const knownHtmlLikeLangs = [
-      'html'
+      'html',
+      'postfix'
     ]
 
     if (knownHtmlLikeLangs.includes(document.languageId)) {
